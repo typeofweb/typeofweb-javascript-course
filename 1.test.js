d88af20d51bcac36589dc8d0fe7179c891f02f98
Michal Miszczyszyn <michal@mmiszy.pl>
2019-11-06 14:46:22 +0100

    Add prettier config

--- 1.test.js
+++ 1.test.js
@@ -5,6 +5,7 @@ describe('Ćwiczenie 1 - nazwa', () => {
   test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
   });
+
   test('adds 2 + 3 to equal 5', () => {
     expect(sum(2, 3)).toBe(5);
   });
